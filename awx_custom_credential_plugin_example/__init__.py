d2663950c16a1adc51776c5444997ee7b78a1601
vmazurukrtelecom <155393582+vmazurukrtelecom@users.noreply.github.com>
2025-02-14 22:52:32 +0200

    Update __init__.py

--- awx_custom_credential_plugin_example/__init__.py
+++ awx_custom_credential_plugin_example/__init__.py
@@ -1,4 +1,7 @@
 import json
+import collections
+
+CredentialPlugin = collections.namedtuple('CredentialPlugin', ['name', 'inputs', 'backend'])
 
 def some_lookup_function(**kwargs):
     # Отримуємо metadata
@@ -30,3 +33,49 @@ def some_lookup_function(**kwargs):
         return value[identifier]
 
     raise ValueError(f'Could not find a value for {identifier}.')
+
+example_plugin = CredentialPlugin(
+    'Example AWX Credential Plugin',
+    # see: https://docs.ansible.com/ansible-tower/latest/html/userguide/credential_types.html
+    # inputs will be used to create a new CredentialType() instance
+    #
+    # inputs.fields represents fields the user will specify *when they create*
+    # a credential of this type; they generally represent fields
+    # used for authentication (URL to the credential management system, any
+    # fields necessary for authentication, such as an OAuth2.0 token, or
+    # a username and password). They're the types of values you set up _once_
+    # in AWX
+    #
+    # inputs.metadata represents values the user will specify *every time
+    # they link two credentials together*
+    # this is generally _pathing_ information about _where_ in the external
+    # management system you can find the value you care about i.e.,
+    #
+    # "I would like Machine Credential A to retrieve its username using
+    # Credential-O-Matic B at identifier=some_key"
+    inputs={
+        'fields': [{
+            'id': 'url',
+            'label': 'Server URL',
+            'type': 'string',
+        }, {
+            'id': 'token',
+            'label': 'Authentication Token',
+            'type': 'string',
+            'secret': True,
+        }],
+        'metadata': [{
+            'id': 'identifier',
+            'label': 'Identifier',
+            'type': 'string',
+            'help_text': 'The name of the key in My Credential System to fetch.'
+        }],
+        'required': ['url', 'token', 'secret_key'],
+    },
+    # backend is a callable function which will be passed all of the values
+    # defined in `inputs`; this function is responsible for taking the arguments,
+    # interacting with the third party credential management system in question
+    # using Python code, and returning the value from the third party
+    # credential management system
+    backend = some_lookup_function
+)
